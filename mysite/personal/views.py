11fa44c7038c144e3f95dc4f88d906f5fad27b36
preksha123 <prekshajagetiya@gmail.com>
2018-11-09 12:09:23 +0530

    My first Django based blog

---
+++ mysite/personal/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'personal/home.html')
+
+
+def contact(request):
+    return render(request, 'personal/basic.html', {'content': ['if you would like to contact, please contact me at', 'jfdjakjcksd@gmail.com']})
+    
